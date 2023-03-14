4b3f24cc1a192c84e07bdcf3c4485cb2754ca62c
Jonasz Gosciniak <127191122+jonozgono@users.noreply.github.com>
2023-03-14 10:40:30 +0100

    FizzBuzz by JonoZgono

---
+++ FizzBuzzJonoZgono.py
@@ -0,0 +1,64 @@
+# parzyste = 0
+# i = 0
+# for i in range(0,4):
+#     i +=1
+#     parzyste +=2
+#     print (parzyste)
+# print("liczb parzystrych:",i)
+
+
+
+# def print_name(first_name, last_name, age):
+#     print(f"Twoje imie to: {first_name} {last_name} of age: {age}")
+#
+# print_name("bartek", age=13, last_name="draczyn")
+
+
+
+# def greet(name):
+#     print(f"Hi {name}")
+#     return True
+# print(greet("czister"))
+
+
+
+# def multiply(*numbers):
+#     print(numbers)
+# multiply(2,3,4,5)
+
+
+
+# def save_user(**user):
+#     print(user)
+# save_user(id = 1, name = "stasiek")
+
+
+
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
+# print(multiply(1,3,4,5))
+
+
+
+def FizzBuzz():
+    l=0
+    for i in range(1,101):
+
+        if (i %3==0 and i %5==0):
+            print("FizzBuzz")
+            l+=1
+        elif (i %3 ==0):
+            print("Fizz")
+            l+=1
+
+        elif (i %5 ==0):
+            print("Buzz")
+
+        else: print(i)
+
+    print("liczby podzilne przez 3 wystapily", l ,"razy")
+
+FizzBuzz()
